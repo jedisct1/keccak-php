c3675da052ad1f63e3e999d108b8974ef672ae5f
Frank Denis <github@pureftpd.org>
2012-10-03 14:35:09 -0700

    Bit vs bytes. Stupid interface.

--- keccak-impl.c
+++ keccak-impl.c
@@ -7,7 +7,7 @@ keccak(unsigned char hash[64U], const unsigned char *message,
        size_t message_size)
 {
     if (Hash(512, (const BitSequence *) message,
-             (DataLength) message_size, hash) != SUCCESS) {
+             (DataLength) message_size * 8ULL, hash) != SUCCESS) {
         return -1;
     }
     return 0;
